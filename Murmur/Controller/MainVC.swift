44cf42dc88b3e209aafe23a47a9339da46726c41
devxris <chuwei0915@gmail.com>
2017-10-25 11:27:49 +0800

    MVC Preparation

---
+++ Murmur/Controller/MainVC.swift
@@ -0,0 +1,15 @@
+//
+//  MainVC.swift
+//  Murmur
+//
+//  Created by Chris Huang on 25/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+
+}
+
